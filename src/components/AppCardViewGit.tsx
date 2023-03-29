a90cfe60ed929372aace6b5e24026d4dc7e3f2ac
Mateus Borges <spekytro68@gmail.com>
2023-03-29 01:06:35 -0300

    ajutes

--- src/components/AppCardViewGit.tsx
+++ src/components/AppCardViewGit.tsx
@@ -12,7 +12,7 @@ import {
 export function AppCardViewGit() {
   const data = [716, 193, 38, 716];
   return (
-    <div className="bg-stone-800 absolute z-10 w-[70%] xl:w-[50%] h-[23vh]  rounded-[30px] bottom-1  xl:bottom-[12vh] p-1 xl:p-3 centralizer flex-col  opacity-75 animation_pulse_sistem ">
+    <div className="bg-stone-800 absolute z-10 w-[70%] xl:w-[50%] h-[23vh]  rounded-[30px] bottom-1  xl:bottom-[12vh] p-1 xl:p-3 centralizer flex-col  opacity-75 animation_pulse_sistem animate-bounce ">
       <div className="flex w-full">
         <UsersFour className="w-5 h-5 text-white" />
         <p className="font-semibold text-[14px] ml-2">0 Seguidores</p>
